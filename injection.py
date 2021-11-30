2bc9bad2340fa27594c910fe0acfd3df28c828df
Kirill Molchanov <Kirill_Molchanov@epam.com>
2021-11-30 15:20:52 +0300

    add injection mechanism

---
+++ injection.py
@@ -0,0 +1,5 @@
+from scrapper.injection import Injector
+
+if __name__ == "__main__":
+    injector = Injector()
+    injector.start()
---
+++ scrapper/injection.py
@@ -0,0 +1,31 @@
+import time
+
+from config import InjectionConfig
+from scrapper.queues import ScrapperQueue
+from db.alchemy_spotify_db import ASpotifyDB as SpotifyDB
+
+from utils.profile import profile
+
+
+class Injector:
+    def __init__(self):
+        self.scrapper_queue = ScrapperQueue()
+
+        self.injection_count = InjectionConfig.INJECTION_COUNT
+        self.injection_delay_sec = InjectionConfig.INJECTION_DELAY_SEC
+        # Init database
+        self.db = SpotifyDB()
+
+    @profile
+    def _inject(self):
+        artists, potential = zip(*self.db.get_relations())
+        artists = set(artists)
+        potential = set(potential)
+        diff = list(potential.difference(artists))[:self.injection_count]
+        for d in diff:
+            self.scrapper_queue.push(d)
+
+    def start(self):
+        while True:
+            self._inject()
+            time.sleep(self.injection_delay_sec)
